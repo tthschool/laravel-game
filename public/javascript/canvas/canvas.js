3a1f885456c735f9ef4cea1ab0ba1d274146356b
hoang <tth251294@gmail.com>
2024-04-24 07:50:05 +0900

    faild to check vertical

--- public/javascript/canvas/canvas.js
+++ public/javascript/canvas/canvas.js
@@ -1,145 +1,184 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
-
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-
-var mouse= {
-    x : undefined,
-    y :undefined,
-}
-
-window.addEventListener("mousemove" , function(e){
-    mouse.x = e.x;
-    mouse.y = e.y;
-})
-class Circle {
-    constructor(x, y, dx, dy, radius, color) {
-        this.x = x;
-        this.y = y;
-        this.dx = dx;
-        this.dy = dy;
-        this.radius = radius;
-        this.color = color;
-    }
-    draw() {
-        c.beginPath();
-        c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-        c.fillStyle = this.color;
-        c.fill();
-        c.stroke();
-    }
-    update() {
-        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
-            this.dx = -this.dx;
-        }
-        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
-            this.dy = -this.dy;
-        }
-        this.x += this.dx;
-        this.y += this.dy;
-        this.draw();
-    }
-}
-
-class Drawline {
+var player = {
+    player1: { x: undefined, y: undefined },
+    player2: { x: undefined, y: undefined }
+};
+var currentplayer = 0;
+class DrawLine {
     constructor(x, y, z, k) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.k = k;
     }
-    draw() {
+    Drawline() {
         c.beginPath();
         c.moveTo(this.x, this.y);
         c.lineTo(this.z, this.k);
         c.stroke();
     }
     update() {
+        this.Drawline();
+    }
+}
+class DrawCircle {
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
+        c.stroke();
+    }
+    update() {
         this.draw();
     }
 }
-var colors = ["#F29F05", "#0A3A40", "#0F5959", "#F20505", "#107361"];
-var circles = [];
-function checkcollision(circle_a, circle_b) {
-    let a = Math.pow(circle_a.x - circle_b.x, 2);
-    let b = Math.pow(circle_a.y - circle_b.y, 2);
-    let c = Math.sqrt(a + b);
-    let totalradius = circle_a.radius + circle_b.radius;
-    if (c < totalradius) {
-        return true;
+class Tictactoe {
+    constructor(gamerow) {
+        this.gamerow = gamerow;
+        this.Board = [];
     }
-}
-function checkcollisionwithmouse(circle,mouse){
-    if(mouse.x - circle.x < 80){
-        return true;
-    }
-}
-
-// Function to handle elastic collision
-function handleCollision(circle1, circle2) {
-    // Calculate relative velocity
-    const relVelX = circle2.dx - circle1.dx;
-    const relVelY = circle2.dy - circle1.dy;
-
-    // Calculate normal vector
-    const normalX = circle2.x - circle1.x;
-    const normalY = circle2.y - circle1.y;
-
-    // Normalize normal vector
-    const normalLength = Math.sqrt(normalX * normalX + normalY * normalY);
-    const unitNormalX = normalX / normalLength;
-    const unitNormalY = normalY / normalLength;
-
-    // Calculate relative velocity along the normal
-    const velAlongNormal = relVelX * unitNormalX + relVelY * unitNormalY;
-
-    // If the circles are moving away from each other, do nothing
-    if (velAlongNormal > 0) {
-        return;
-    }
-
-    // Calculate impulse scalar
-    const impulse =
-        (-2 * velAlongNormal) / (1 / circle1.radius + 1 / circle2.radius);
-
-    // Apply impulse to circles
-    circle1.dx -= (impulse / circle1.radius) * unitNormalX;
-    circle1.dy -= (impulse / circle1.radius) * unitNormalY;
-    circle2.dx += (impulse / circle2.radius) * unitNormalX;
-    circle2.dy += (impulse / circle2.radius) * unitNormalY;
-}
-
-function init() {
-    for (let i = 0; i < 10; i++) {
-        var radius = 30;
-        var x = Math.random() * (window.innerWidth - radius * 2) + radius;
-        var y = Math.random() * (window.innerHeight - radius * 2) + radius;
-        var dx = Math.round(Math.random() * 5 + 1);
-        var dy = Math.round(Math.random() * 5 + 1);
-        var colorIndex = Math.floor(Math.random() * colors.length);
-        var color = colors[colorIndex];
-        var circle = new Circle(x, y, dx, dy, radius, color);
-        circles.push(circle);
-    }
-}
-function animate() {
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < circles.length; i++) {
-        circles[i].update();
-    }
-    for (let i = 0; i < circles.length; i++) {
-        for (let j = i + 1; j < circles.length; j++) {
-            if (checkcollision(circles[i], circles[j])) {
-                handleCollision(circles[i], circles[j]);
+    CreateBroad() {
+        for (let i = 0; i < this.gamerow - 1; i++) {
+            this.Board[i] = [];
+            for (let j = 0; j < this.gamerow - 1; j++) {
+                this.Board[i][j] = 0;
             }
         }
-        if(checkcollisionwithmouse(circles[i] , mouse)){
-            circles[i].color = "black"
+    }
+    checkHorizontal() {
+        var iswin = false;
+        var count = 0;
+        for (let i = 0; i < this.Board.length; i++) {
+            for (let j = 0; j < this.Board.length; j++) {
+                if (this.Board[i][j] == 1) {
+                    count += 1;
+                } else {
+                    if (count == 3) {
+                        iswin = true;
+                    } else {
+                        count = 0;
+                    }
+                }
+            }
         }
+        return iswin;
+    }
+    checkVertical() {
+        var count = 0;
+        for (let i = 0; i < this.Board.length; i++) {
+            for (let j = 0; j < this.Board.length; j++) {
+                if (this.Board[j][i] == 1) {
+                    count += 1;
+                }
+                else {
+                    if (count == 3) {
+                        return true;
+                    } else {
+                        count = 0;
+                    }
+                }
+            }
+        }
+    }
+    checkmaindiagonal() {
+        var count = 0;
+        for (let i = 0; i < this.Board.length; i++) {
+            if (this.Board[i][i] == 1) {
+                count += 1;
+            } else {
+                count = 0;
+            }
+            // check if cn
+            if (count == 3) {
+                return true;
+            }
+        }
+        // Trả về false nếu không tìm thấy chuỗi ba "1" liên tiếp
+        return false;
+    }
 
+    checksub() {
+        var count = 0;
+        for (let i = 0; i < this.Board.length; i++) {
+            if (this.Board[i][this.gamerow - i - 1] == 1) { // Sửa gamerow thành this.gamerow
+                count += 1;
+            } else {
+                count = 0;
+            }
+            // Kiểm tra ngay khi đếm đủ 3
+            if (count == 3) {
+                return true;
+            }
+        }
+        // Trả về false nếu không tìm thấy chuỗi ba "1" liên tiếp
+        return false;
+    }
+
+    checkwinner() {
+        console.log(this.checksub());
+        if (
+            this.checkHorizontal() ||
+            this.checkVertical() ||
+            this.checkmaindiagonal() ||
+            this.checksub()
+        ) {
+            console.log("win");
+        }
+    }
+    showBroad() {
+        this.CreateBroad();
+        return this.Board;
     }
 }
-init();
-animate();
+var mouse = {
+    x: undefined,
+    y: undefined,
+};
+const gamerow = 4;
+const rowWidth = 100;
+for (let i = 1; i <= gamerow; i++) {
+    var line = new DrawLine(100, 100 * i, 400, 100 * i);
+    line.Drawline();
+    var line2 = new DrawLine(100 * i, 100, 100 * i, 400);
+    line2.Drawline();
+}
+//create board
+var game = new Tictactoe(gamerow);
+var board = game.showBroad();
+function drawX(posx, posy) {
+    c.font = "60px Arial";
+    c.fillText("X", posx, posy);
+}
+function drawO(posx, posy) {
+    c.font = "60px Arial";
+    c.fillText("0", posx, posy);
+}
+window.addEventListener("click", function (e) {
+    mouse.x = e.x;
+    mouse.y = e.y;
+    console.log(mouse);
+    var positionx = Math.floor((mouse.y - 100) / rowWidth);
+    var positiony = Math.floor((mouse.x - 100) / rowWidth);
+    board[positionx][positiony] = 1;
+    var posx = Math.floor(mouse.x / 100) * 100 + 30;
+    var posy = Math.floor(mouse.y / 100) * 100 + 70;
+    game.checkwinner();
+
+    if (currentplayer == 0) {
+        drawX(posx, posy);
+        currentplayer = 1;
+    }
+    else{
+        drawO(posx , posy);
+        currentplayer = 0
+    };
+});
+
+
--- resources/views/dotgames.blade.php
+++ resources/views/dotgames.blade.php
@@ -5,6 +5,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Elastic Collision</title>
 <style>
+    body{
+        margin: 0;
+    }
     canvas {
         border: 1px solid black;
     }
