6452ce86d52c02d286948920990824dfe669a3a6
hoang <tth251294@gmail.com>
2024-04-25 22:28:03 +0900

    done

--- public/javascript/canvas/canvas.js
+++ public/javascript/canvas/canvas.js
@@ -2,11 +2,9 @@ const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 500;
-const gamerow = 4;
-const rowWidth = 50;
-const rowHeight = 50 ;
+
 var gameover = false;
-const wincondition = 3 ;
+const wincondition = 3;
 var mouse = {
     x: undefined,
     y: undefined,
@@ -14,8 +12,9 @@ var mouse = {
 var currentplayer = "o";
 var player = {
     player1: { x: undefined, y: undefined },
-    player2: { x: undefined, y: undefined }
+    player2: { x: undefined, y: undefined },
 };
+
 class DrawLine {
     constructor(x, y, z, k) {
         this.x = x;
@@ -49,28 +48,31 @@ class DrawCircle {
     }
 }
 class Tictactoe {
-    constructor(gamerow , currentplayer) {
-        this.gamerow = gamerow;
+    constructor(gamesize) {
+        this.gamesize = gamesize;
         this.Board = [];
-        this.currentplayer= currentplayer;
+        this.currentplayer = currentplayer;
+    }
+    getcurrentplayer() {
+        console.log(this.currentplayer);
     }
     CreateBroad() {
-        for (let i = 0; i < this.gamerow - 1; i++) {
+        for (let i = 0; i < this.gamesize; i++) {
             this.Board[i] = [];
-            for (let j = 0; j < this.gamerow - 1; j++) {
-                this.Board[i][j] = undefined;
+            for (let j = 0; j < this.gamesize; j++) {
+                this.Board[i][j] = null;
             }
         }
     }
     checkHorizontal() {
+        console.log(this.Board.length);
         var count = 0;
         for (let i = 0; i < this.Board.length; i++) {
             for (let j = 0; j < this.Board.length; j++) {
                 if (this.Board[i][j] === this.currentplayer) {
                     count += 1;
-                }
-                else{
-                    count = 0 ;
+                } else {
+                    count = 0;
                 }
                 if (count === wincondition) {
                     return true;
@@ -79,57 +81,49 @@ class Tictactoe {
         }
     }
     checkVertical() {
-        console.log(this.currentplayer);
         var count = 0;
         for (let i = 0; i < this.Board.length; i++) {
             for (let j = 0; j < this.Board.length; j++) {
                 if (this.Board[j][i] === this.currentplayer) {
                     count += 1;
+                } else {
+                    count = 0;
                 }
-                else{
-                    count = 0 ;
-                }
-                if(count === wincondition){
+                if (count === wincondition) {
                     return true;
                 }
-
             }
         }
     }
-    checkmaindiagonal() {
-        var count = 0;
+    checkDiagonals() {
+        var count1 = 0;
         for (let i = 0; i < this.Board.length; i++) {
             if (this.Board[i][i] === this.currentplayer) {
-                count += 1;
+                console.log(`gia tri = ${this.Board[i][i]} ,tai  i = ${i}  `);
+                console.log(this.Board);
+                count1 += 1;
             } else {
-                count = 0;
-            }
-            if (count == wincondition) {
-                console.log("main");
-                return true;
+                count1 = 0;
             }
         }
-    }
-    checksub(){
-        var count = 0;
-        for (let i = 0; i < this.Board.length; i++) {
-            if (this.Board[i][this.gamerow - i - 2] === this.currentplayer) {
-                count += 1;
+        var count2 = 0;
+        for (let i = 0; i < this.Board.length-1; i++) {
+            if (this.Board[i][this.gamesize - i - 1] === this.currentplayer) {
+                count2 += 1;
             } else {
-                count = 0;
-            }
-            if (count == wincondition) {
-                return true;
+                count2 = 0;
             }
         }
+        if (count1 === wincondition || count2 === wincondition) {
+            return true;
+        }
     }
+
     checkwinner() {
         if (
             this.checkHorizontal() ||
-            this.checkVertical() ||
-            this.checkmaindiagonal() ||
-            this.checksub()
-
+            this.checkVertical()||
+            this.checkDiagonals()
         ) {
             return true;
         }
@@ -139,57 +133,82 @@ class Tictactoe {
         return this.Board;
     }
 }
-for (let i = 1; i <= gamerow; i++) {
-    var line = new DrawLine(rowHeight, rowHeight * i, gamerow*rowWidth, rowHeight * i);
-    line.Drawline();
-    var line2 = new DrawLine(rowHeight * i, rowHeight, rowHeight * i, gamerow*rowWidth);
-    line2.Drawline();
+var startx = 50;
+var starty = 50;
+var lastpointx = 50;
+var lastpointy = 50;
+var displaysize = 10;
+
+var gamesize = displaysize - 1;
+var rowsize = 50;
+for (let i = 1; i <= displaysize; i++) {
+    var line = new DrawLine(
+        startx,
+        starty * i,
+        lastpointx * displaysize,
+        lastpointy * i
+    );
+    line.update();
+    var line2 = new DrawLine(
+        startx * i,
+        starty,
+        lastpointx * i,
+        lastpointy * displaysize,
+    );
+    line2.update();
 }
 //create board
-var game = new Tictactoe(gamerow , currentplayer);
+var game = new Tictactoe(gamesize);
 var board = game.showBroad();
 function drawX(posx, posy) {
     c.font = "50px Arial";
-    c.fillText("X", posx, posy);
+    c.fillStyle = "red";
+    c.fillText("x", posx, posy);
 }
 function drawO(posx, posy) {
     c.font = "50px Arial";
-    c.fillText("0", posx, posy);
+    c.fillStyle = "blue";
+    c.fillText("o", posx, posy);
 }
 function drawwineer(winner) {
-    c.font = "60px Arial";
+    c.font = "100px Arial";
+    c.fillStyle = "black";
     c.fillText(`${winner} win`, 190, 260);
 }
 
 window.addEventListener("click", function (e) {
     if (gameover) {
-        return ;
+        return;
     }
     mouse.x = e.x;
     mouse.y = e.y;
-    var positionx = Math.floor((mouse.y - rowHeight) / rowWidth);
-    var positiony = Math.floor((mouse.x - rowHeight) / rowHeight);
-    console.log(positionx);
-    var posx = Math.floor(mouse.x / rowHeight) * rowHeight + 10;
-    var posy = Math.floor(mouse.y / rowHeight) * rowHeight + 45;
-    if (board[positionx][positiony] === undefined) {
-        board[positionx][positiony] = currentplayer
-        board.currentplayer = currentplayer;
+    var positionx = Math.floor((mouse.y - startx) / rowsize);
+    var positiony = Math.floor((mouse.x - starty) / rowsize);
+    var posx = Math.floor(mouse.x / rowsize) * rowsize + 10;
+    var posy = Math.floor(mouse.y / rowsize) * rowsize + 45;
+    if (board[positionx][positiony] === null) {
+        board[positionx][positiony] = currentplayer;
+        game.currentplayer = currentplayer;
+        if (game.checkwinner()) {
+            drawwineer(currentplayer);
+            gameover = true;
+        }
         if (currentplayer === "x") {
             drawX(posx, posy);
             currentplayer = "o";
+
+        } else {
+            drawO(posx, posy);
+            currentplayer = "x";
         }
-        else{
-            drawO(posx , posy);
-            currentplayer = "x"
-        };
-        if(game.checkwinner()){
-            drawwineer(currentplayer)
-            gameover = true;
-        };
-        console.log(game);
+
     }
 });
 
-
-//current player always be O
+var test = []
+for (let i = 0; i < this.gamesize; i++) {
+    this.test[i] = [];
+    for (let j = 0; j < this.gamesize; j++) {
+        this.test[i][j] = undefined;
+    }
+}
